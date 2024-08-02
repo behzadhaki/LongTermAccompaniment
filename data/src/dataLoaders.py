Fixed causal mask and added random src masking for teacher forcing

data/src/dataLoaders.py
@@ -486,29 +486,23 @@ def upsample_to_ensure_genre_balance(dataset_setting_json_path, subset_tag, cach
         return set_data_
 
 
-class PairedLTADataset(Dataset):
+class PairedLTADatasetV2(Dataset):
     def __init__(self,
                  input_inst_dataset_bz2_filepath,
                  output_inst_dataset_bz2_filepath,
                  shift_tgt_by_n_steps=1,
                  max_input_bars=32,
-                 continuation_bars=2,
                  hop_n_bars=2,
-                 input_has_velocity=True,
-                 input_has_offsets=True,
                  push_all_data_to_cuda=False):
 
         self.max_input_bars = max_input_bars
-        self.continuation_bars = continuation_bars
         self.hop_n_bars = hop_n_bars
-        self.input_has_velocity = input_has_velocity
-        self.input_has_offsets = input_has_offsets
 
         def get_cached_filepath():
             dir_ = "cached/TorchDatasets"
             filename = (
-                f"PairedLTADataset_{input_inst_dataset_bz2_filepath.split('/')[-1]}_{output_inst_dataset_bz2_filepath.split('/')[-1]}"
-                f"_{max_input_bars}_{hop_n_bars}_{input_has_velocity}_{input_has_offsets}_{shift_tgt_by_n_steps}.bz2pickle")
+                f"PairedLTADatasetV2_{input_inst_dataset_bz2_filepath.split('/')[-1]}_{output_inst_dataset_bz2_filepath.split('/')[-1]}"
+                f"_{max_input_bars}_{hop_n_bars}_{shift_tgt_by_n_steps}.bz2pickle")
             if not os.path.exists(dir_):
                 os.makedirs(dir_)
             return os.path.join(dir_, filename)
@@ -551,9 +545,30 @@ class PairedLTADataset(Dataset):
             self.instrument1_hvos = torch.tensor(data["instrument1_hvos"], dtype=torch.float32)
             self.instrument2_hvos = torch.tensor(data["instrument2_hvos"], dtype=torch.float32)
             self.instrument1and2_hvos = torch.tensor(data["instrument1and2_hvos"], dtype=torch.float32)
+            self.instrument2_shifted_hvos = torch.tensor(data["instrument2_shifted_hvos"], dtype=torch.float32)
 
         else:
             should_process_data = True
+
+        def get_hit_counts_per_bar(hvo_in):
+            counts = []
+            n_features = hvo_in.shape[-1] // 3
+            for i in range(0, hvo_in.shape[0], 16):
+                counts.append(hvo_in[i:i+16, :n_features].sum())
+            return counts
+
+        def is_valid(hvo_groove, hvo_drum):
+            groove_counts = get_hit_counts_per_bar(hvo_groove)
+            drum_counts = get_hit_counts_per_bar(hvo_drum)
+
+            # invalid if first groove bar has no hits or drum hits
+            # invalid if more than 2 empty groove bars or more than 2 empty drum bars
+            if groove_counts[0] == 0 or drum_counts[0] == 0:
+                return False
+            elif groove_counts.count(0) > 2 or drum_counts.count(0) > 2:
+                return False
+            else:
+                return True
 
         if should_process_data:
             # load data
@@ -569,8 +584,7 @@ class PairedLTADataset(Dataset):
             inst1_hvos = []
             inst2_hvos = []
             inst1and2_hvos = []
-
-            max_segment_len_bars = max_input_bars + continuation_bars
+            inst2_shifted_hvos = []
 
             for song_id in tqdm(song_ids_all):
                 i1 = instrument1s[song_id]
@@ -593,25 +607,30 @@ class PairedLTADataset(Dataset):
 
                 for i in segments_starts:
                     ts_ = i * 16
-                    te_ = ts_ + max_segment_len_bars * 16
+                    te_ = ts_ + max_input_bars * 16
                     i1_seg = i1.hvo[ts_:te_]
                     i2_seg = i2.hvo[ts_:te_]
 
-                    if i1_seg.shape[0] == max_segment_len_bars * 16:
-                        i1_2_stack = stack_two_hvos(i1_seg, i2_seg, input_has_velocity, input_has_offsets)
+                    if i1_seg.shape[0] == max_input_bars * 16 and is_valid(i1_seg, i2_seg):
+                        i1_2_stack = stack_two_hvos(i1_seg, i2_seg, True, True)
                         inst1_hvos.append(i1_seg)
                         inst2_hvos.append(i2_seg)
                         inst1and2_hvos.append(i1_2_stack)
+                        inst2_shifted_ = np.zeros_like(i2_seg)
+                        inst2_shifted_[shift_tgt_by_n_steps:] = i2_seg[:-shift_tgt_by_n_steps]
+                        inst2_shifted_hvos.append(inst2_shifted_)
 
             self.instrument1_hvos = torch.vstack([torch.tensor(x, dtype=torch.float32).unsqueeze(0) for x in inst1_hvos])
             self.instrument2_hvos = torch.vstack([torch.tensor(x, dtype=torch.float32).unsqueeze(0) for x in inst2_hvos])
             self.instrument1and2_hvos = torch.vstack([torch.tensor(x, dtype=torch.float32).unsqueeze(0) for x in inst1and2_hvos])
+            self.instrument2_shifted_hvos = torch.vstack([torch.tensor(x, dtype=torch.float32).unsqueeze(0) for x in inst2_shifted_hvos])
 
             # cache the processed data
             data_to_dump = {
                 "instrument1_hvos": self.instrument1_hvos.numpy(),
                 "instrument2_hvos": self.instrument2_hvos.numpy(),
                 "instrument1and2_hvos": self.instrument1and2_hvos.numpy(),
+                "instrument2_shifted_hvos": self.instrument2_shifted_hvos.numpy(),
             }
 
             ofile = bz2.BZ2File(get_cached_filepath(), 'wb')
@@ -625,6 +644,7 @@ class PairedLTADataset(Dataset):
             self.instrument1_hvos = self.instrument1_hvos.cuda()
             self.instrument2_hvos = self.instrument2_hvos.cuda()
             self.instrument1and2_hvos = self.instrument1and2_hvos.cuda()
+            self.instrument2_shifted_hvos = self.instrument2_shifted_hvos.cuda()
 
     def get_hit_density_histogram(self, n_bins=100):
         hit_densities = []
@@ -644,9 +664,10 @@ class PairedLTADataset(Dataset):
         return len(self.instrument1_hvos)
 
     def __getitem__(self, idx):
-        return (self.instrument1_hvos[idx],  # 0: instrument1_hvos (shape: (max_input_bars + continuation_bars, 3 * features_inst_1))
-                self.instrument2_hvos[idx],  # 1: instrument2_hvos (shape: (max_input_bars + continuation_bars, 3 * features_inst_2))
-                self.instrument1and2_hvos[idx]  # 2: instrument1and2_hvos (shape: (max_input_bars + continuation_bars, 3 * features_inst_1 + 3 * features_inst_2))
+        return (self.instrument1_hvos[idx],
+                self.instrument2_hvos[idx],
+                self.instrument1and2_hvos[idx],
+                self.instrument2_shifted_hvos[idx]
                 )
 
 

model/LTA/__init__.py
@@ -315,7 +315,7 @@ class DrumDecoder(torch.nn.Module):
                 segment_length=self.performance_encoder_input_steps['steps_per_segment'])
 
         @torch.jit.export
-        def forward(self, tgt: torch.Tensor, memory: torch.Tensor):
+        def forward(self, tgt: torch.Tensor, memory: torch.Tensor, teacher_forcing_ratio: float=1.0):
             out_with_position, hit, hvo_projection = self.InputLayerEncoder(hvo=tgt)
 
             # replace True to -inf, False to 0
@@ -326,11 +326,25 @@ class DrumDecoder(torch.nn.Module):
             if self.memory_mask.device != memory.device:
                 self.memory_mask = self.memory_mask.to(memory.device)
 
+            if teacher_forcing_ratio < 0.98:
+                indices = torch.rand(self.causal_mask.size(0))
+                indices = indices > teacher_forcing_ratio
+                causal_mask = self.causal_mask.clone()
+                causal_mask[:, indices] = True
+
+                indices = torch.rand(self.memory_mask.size(0))
+                indices = indices > teacher_forcing_ratio
+                memory_mask = self.memory_mask.clone()
+                memory_mask[:, indices] = True
+            else:
+                causal_mask = self.causal_mask
+                memory_mask = self.memory_mask
+
             output = self.Decoder(
                 tgt=out_with_position,
                 memory=memory,
-                tgt_mask=self.causal_mask[:tgt.shape[1], :tgt.shape[1]],
-                memory_mask=self.memory_mask[:tgt.shape[1], :memory.shape[1]]
+                tgt_mask=causal_mask[:tgt.shape[1], :tgt.shape[1]],
+                memory_mask=memory_mask[:tgt.shape[1], :memory.shape[1]]
             )
 
             return self.HitOutputLayer(output), self.VelocityOutputLayer(output), self.OffsetOutputLayer(output)
@@ -862,7 +876,7 @@ class LTA(torch.nn.Module):
     def unfreeze_decoder(self):
         self.DrumDecoder.requires_grad_(True)
 
-    def forward(self, src: torch.Tensor, shifted_tgt: torch.Tensor):
+    def forward(self, src: torch.Tensor, shifted_tgt: torch.Tensor, teacher_forcing_ratio: float=1.0):
 
         # StepEncoder
         n_segments = int(src.shape[1] // self.n_steps_per_segment)
@@ -879,7 +893,8 @@ class LTA(torch.nn.Module):
         # decode the output groove
         h_logits, v_logits, o_logits = self.DrumDecoder.forward(
             tgt=shifted_tgt,
-            memory=perf_encodings)
+            memory=perf_encodings,
+            teacher_forcing_ratio=teacher_forcing_ratio)
 
         return h_logits, v_logits, o_logits
 

model/LTA_Stacked/__init__.py
@@ -143,11 +143,13 @@ class LTA_Stacked(torch.nn.Module):
             num_layers=self.config['n_layers'],
             norm=norm_encoder)
 
-        self.causal_mask = generate_memory_mask_for_K_bars_ahead_prediction(
-            input_steps=self.config['max_n_steps'],
-            output_steps=self.config['max_n_steps'],
-            predict_K_bars_ahead=self.predict_K_bars_ahead,
-            segment_length=1)
+        # self.causal_mask = generate_memory_mask_for_K_bars_ahead_prediction(
+        #     input_steps=self.config['max_n_steps'],
+        #     output_steps=self.config['max_n_steps'],
+        #     predict_K_bars_ahead=self.predict_K_bars_ahead,
+        #     segment_length=1)
+
+        self.causal_mask = torch.nn.Transformer.generate_square_subsequent_mask(self.config['max_n_steps'])
 
         self.HitOutputLayer = SingleFeatureOutputLayer(
             embedding_size=self.n_total_voices,
@@ -177,10 +179,15 @@ class LTA_Stacked(torch.nn.Module):
         # StepEncoder
         n_steps = int(shifted_tgt.shape[1])
 
-        encoded_steps = torch.zeros((shifted_tgt.shape[0], n_steps, self.encoder_d_model), device=shifted_tgt.device)
-        for i in range(n_steps):
-            encoded_steps[:, i, :] = self.StepEncoder(
-                src=shifted_tgt[:, i, :])
+        batch_size, n_steps, encoder_d_model = shifted_tgt.shape
+        shifted_tgt_reshaped = shifted_tgt.view(-1, encoder_d_model)
+        encoded_steps_reshaped = self.StepEncoder(src=shifted_tgt_reshaped)
+        encoded_steps = encoded_steps_reshaped.view(batch_size, n_steps, -1)
+        #
+        # encoded_steps = torch.zeros((shifted_tgt.shape[0], n_steps, self.encoder_d_model), device=shifted_tgt.device)
+        # for i in range(n_steps):
+        #     encoded_steps[:, i, :] = self.StepEncoder(
+        #         src=shifted_tgt[:, i, :])
 
         # TransformerEncoder
         src = self.PositionalEncoding(encoded_steps)
@@ -195,7 +202,47 @@ class LTA_Stacked(torch.nn.Module):
         o_logits = self.OffsetOutputLayer(encodings)
 
         return h_logits, v_logits, o_logits
+    #forward_using_batch_data_teacher_force
 
+    @torch.jit.ignore
+    def forward_src_masked(self, shifted_tgt: torch.Tensor, teacher_forcing_ration: float=0.9):
+        # used for training only
+
+        # StepEncoder
+        n_steps = int(shifted_tgt.shape[1])
+
+        if teacher_forcing_ration < 1.0:
+            indices = torch.rand(self.causal_mask.size(0))
+            indices = indices > teacher_forcing_ration
+            causal_mask = self.causal_mask.clone()
+            causal_mask[:, indices] = True
+        else:
+            causal_mask = self.causal_mask
+
+        batch_size, n_steps, encoder_d_model = shifted_tgt.shape
+        shifted_tgt_reshaped = shifted_tgt.view(-1, encoder_d_model)
+        encoded_steps_reshaped = self.StepEncoder(src=shifted_tgt_reshaped)
+        encoded_steps = encoded_steps_reshaped.view(batch_size, n_steps, -1)
+        #
+        # encoded_steps = torch.zeros((shifted_tgt.shape[0], n_steps, self.encoder_d_model), device=shifted_tgt.device)
+        # for i in range(n_steps):
+        #     encoded_steps[:, i, :] = self.StepEncoder(
+        #         src=shifted_tgt[:, i, :])
+
+        # TransformerEncoder
+        src = self.PositionalEncoding(encoded_steps)
+
+        encodings = self.TransformerEncoder.forward(
+            src=src,
+            is_causal=True,
+            mask=causal_mask[:src.shape[1], :src.shape[1]])
+
+        # decode the output groove
+        h_logits = self.HitOutputLayer(encodings)
+        v_logits = self.VelocityOutputLayer(encodings)
+        o_logits = self.OffsetOutputLayer(encodings)
+
+        return h_logits, v_logits, o_logits
 
     @torch.jit.ignore
     def sample(self, shifted_tgt: torch.Tensor, scale_vel: float=1.0, threshold: float=0.5, use_bernulli: bool=False, temperature: float=1.0):

trainings_LTA/train_LTA_1bar_lookahead.py
@@ -331,32 +331,11 @@ if __name__ == "__main__":
 
         enc_src = bass_solo.to(device) if bass_solo.device.type != device else bass_solo
 
-        if config['teacher_forcing_ratio'] >= 0.95:
-            h_logits, v_log, o_log = model_.forward(
-                src=enc_src,
-                shifted_tgt=shifted_drums.to(device) if shifted_drums.device.type != device else shifted_drums)
-            return h_logits, v_log, o_log, drums.to(device)
-        # else:
-        #     shifted_predicted_tgt = torch.zeros((drums.shape[0], drums.shape[1] + 16, drums.shape[2])).to(device)
-
-        #     n_4_bars = shifted_predicted_tgt.shape[1] // (16)
-        #
-        #     for i in range(n_4_bars):
-        #         h_logits, v_log, o_log = model_.forward(
-        #             src=enc_src[:, :16*(i+1), :],
-        #             shifted_tgt=shifted_predicted_tgt[:, :16*(i+1), :]
-        #         )
-        #         if torch.rand(1).item() > config['teacher_forcing_ratio']:
-        #             h = torch.sigmoid(h_logits[:, -16:, :])
-        #             # bernoulli sampling
-        #             v = torch.clamp((torch.tanh(v_log[:, -16:, :]) + 1.0) / 2.0, 0.0, 1.0)
-        #             o = torch.tanh(o_log[:, -16:, :])
-        #             shifted_predicted_tgt[:, i*16:(i+1)*16, :] = torch.cat((h, v, o), dim=-1)
-        #             del h, v, o
-        #         else:
-        #             shifted_predicted_tgt[:, i*16:(i+1)*16, :] = shifted_drums[:, i*16:(i+1)*16, :].to(device)
-
-        # return h_logits, v_log, o_log, drums.to(device)
+        h_logits, v_log, o_log = model_.forward(
+            src=enc_src,
+            shifted_tgt=shifted_drums.to(device) if shifted_drums.device.type != device else shifted_drums,
+            teacher_forcing_ratio=config.teacher_forcing_ratio)
+        return h_logits, v_log, o_log, drums.to(device)
 
 
     for epoch in range(config.epochs):